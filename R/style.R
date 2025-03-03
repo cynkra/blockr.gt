2840dc6773a6a9f6ba2ebaacff9bcad57c097839
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-03-03 16:58:24 +0100

    Add server logic

--- R/style.R
+++ R/style.R
@@ -20,19 +20,59 @@ new_style_gt_block <- function(
         selected = "gray"
       ),
       selectInput(
-        NS(id, "add_row_striping"),
+        NS(id, "striping"),
         label = "Should rows be striped?",
         choices = c("yes", "no"),
         selected = "yes"
+      ),
+      gt_output(
+        NS(id, "table")
       )
     )
   }
 
   server <- function(id, gt_obj) {
     moduleServer(id, function(input, output, session) {
+      # Initialise module with ctor values
+      style <- reactiveVal(style)
+      color <- reactiveVal(color)
+      striping <- reactiveVal(striping)
+
+      # Update values from the UI
+      observeEvent(input$style, style(input$style))
+      observeEvent(input$color, color(input$color))
+      observeEvent(input$striping, striping(input$striping))
+
+      output$table <- render_gt({
+        gt(gt_obj()) |>
+          opt_stylize(
+            style = style(),
+            color = color(),
+            add_row_striping = ifelse(striping() == "yes", TRUE, FALSE)
+          )
+      })
+
       list(
-        expr = list(),
-        state = list()
+        expr = reactive(
+          bquote(
+            gt(gt_obj()) |>
+              opt_stylize(
+                style = .(style),
+                color = .(color),
+                add_row_striping = ifelse(.(striping) == "yes", TRUE, FALSE)
+              ),
+            list(
+              style = style(),
+              color = color(),
+              striping = striping()
+            )
+          )
+        ),
+        state = list(
+          style = reactive(style()),
+          color = reactive(color()),
+          striping = reactive(striping())
+        )
       )
     })
   }
