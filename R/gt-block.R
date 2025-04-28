154f4a3a8c484d6a44d9b05720c0bbc7f4ea2502
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-04-28 19:12:03 +0200

    Add templates for ui/output generics

--- R/gt-block.R
+++ R/gt-block.R
@@ -1,3 +1,21 @@
+#' @keywords internal
+#' @noRd
 new_gt_block <- function(server, ui, class, ctor = sys.parent(), ...) {
-    new_block(server, ui, c(class, "gt_block"), ctor, ...)
+  new_block(server, ui, c(class, "gt_block"), ctor, ...)
+}
+
+#' @keywords internal
+#' @noRd
+block_output.gt_block <- function(x, result, session) {
+  gt_result <- attr(result, "gt_tbl")
+  req(gt_result)
+  render_gt(gt_result)
+}
+
+#' @keywords internal
+#' @noRd
+block_ui.gt_block <- function(id, x, ...) {
+  tagList(
+    gt_output(NS(id, "result"))
+  )
 }
