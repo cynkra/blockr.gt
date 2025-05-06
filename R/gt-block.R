f33f9961421c916ed4c13b58ede74eea570a2f67
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-05-06 18:48:03 +0200

    Export generics

--- R/gt-block.R
+++ R/gt-block.R
@@ -4,14 +4,12 @@ new_gt_block <- function(server, ui, class, ctor = sys.parent(), ...) {
   new_block(server, ui, c(class, "gt_block"), ctor, ...)
 }
 
-#' @keywords internal
-#' @noRd
+#' @export
 block_output.gt_block <- function(x, result, session) {
   render_gt(result)
 }
 
-#' @keywords internal
-#' @noRd
+#' @export
 block_ui.gt_block <- function(id, x, ...) {
   tagList(
     gt_output(NS(id, "result"))
